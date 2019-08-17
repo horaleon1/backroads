50569fd146cf8ce882c2a142b93f4f2f276653ef
horaleon1 <horaleon1@gmail.com>
2019-08-16 21:17:40 -0500

    all pages created

---
+++ src/pages/tours.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class tours extends Component {
+  render() {
+    return (
+      <div>
+        helloo from tours page
+      </div>
+    )
+  }
+}
