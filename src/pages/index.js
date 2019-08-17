fb427d4089c9ddcada968825b924d389a182c257
horaleon1 <horaleon1@gmail.com>
2019-08-16 22:03:12 -0500

    Add layout css and few changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default () => (
  
   <Layout>
     Hello world! <Link to="/blog/">blog page </Link>
+    <h1 style={{ fontSize: '20px', textTransform: 'capitalize', color: 'red' }} > hello styling </h1>
   </Layout>
  
 )
