2cd5df7c3f2a6c70ab82c4b69a7208f32f86f4c7
horaleon1 <horaleon1@gmail.com>
2019-08-25 19:00:03 -0500

    "error page done"

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Layout from '../components/Layout';
-
+import styles from '../css/error.module.css';
+import { Link } from 'gatsby';
+import Banner from '../components/Banner';
 
 export default function error() {
   return (
     <Layout>
-      hello from 404
+      <header className={styles.error}>
+       <Banner title='oops it´s a dead end'>
+         <Link to='/' className="btn-white"> 
+         back to home page
+         </Link>
+       </Banner>
+      </header>
     </Layout>
   )
 }
