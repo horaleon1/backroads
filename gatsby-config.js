e2b17d3fb2a0cf6a2bf4e1274a8e06fb225be816
horaleon1 <horaleon1@gmail.com>
2019-08-25 23:06:03 -0500

    gatsby-config data

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata:{
+   title:'BackRoads',
+   description:'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+   author:'@horaleon1',
+   data:{
+     name:'John',
+     age:24,
+   },
+  },
+  plugins: ['gatsby-plugin-styled-components'],
+}
